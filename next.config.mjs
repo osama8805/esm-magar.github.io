425d901813787fa38b5ccb6958e9289041b25c2a
osama8805 <186003194+osama8805@users.noreply.github.com>
2026-03-02 00:21:18 -0500

    Fix basePath and assetPrefix for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/esm-magar.github.io',
+  assetPrefix: '/esm-magar.github.io/',
   typescript: {
     ignoreBuildErrors: true,
   },
